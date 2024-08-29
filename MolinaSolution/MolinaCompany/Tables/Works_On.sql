31083f55c5aa4ef73dc76856cac7ca222827a5b9
trishia <trishiavillaflor092303@gmail.com>
2024-08-30 03:58:57 +0800

    done

--- MolinaSolution/MolinaCompany/Tables/Works_On.sql
+++ MolinaSolution/MolinaCompany/Tables/Works_On.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[Works_On]
 (	
-	[ESSN]	char(9)			Not NUll Constraint [PK_Works_SSN] Primary Key,
-	Pno		int				Not Null Constraint [PK_Works_Number] Primary Key,	
-	[hours] decimal(3,1)	Not Null,	
+	[ESSN]	char(9)			NOT NULL CONSTRAINT [PK_Works_SSN]	  PRIMARY KEY,
+	Pno		int				NOT NULL CONSTRAINT [PK_Works_Number] PRIMARY KEY,	
+	[hours] decimal(3,1)	NOT NULL,	
 
-	Constraint [FK_Works_SSN] Foreign Key (ESSN) References Employee(SSN),
-	Constraint [FK_Works_Number] Foreign Key (Pno) References Project(Pnumber)
+	CONSTRAINT [FK_Works_SSN]	 FOREIGN KEY (ESSN) REFERENCES Employee(SSN),
+	CONSTRAINT [FK_Works_Number] FOREIGN KEY (Pno)  REFERENCES Project(Pnumber)
 );
 
 GO
 
-create nonclustered index [IX_Works_Number] ON[Works_on] ([Pno] ASC)
+CREATE NONCLUSTERED INDEX [IX_Works_Number] ON[Works_on] ([Pno] ASC)
