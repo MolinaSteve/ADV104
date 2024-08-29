3e5f35630fbd869edd9683a96e293148d79df261
trishia <trishiavillaflor092303@gmail.com>
2024-08-29 19:38:18 +0800

    Addition Index

--- MolinaSolution/ActivityPan/Tables/Works_On.sql
+++ MolinaSolution/ActivityPan/Tables/Works_On.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Works_On]
 (	
-	[ESSN]	char(9)			Not NUll Primary Key,
-	Pno		int				Not Null Primary Key,	
+	[ESSN]	char(9)			Not NUll Constraint [PK_Works_SSN] Primary Key,
+	Pno		int				Not Null Constraint [PK_Works_Number] Primary Key,	
 	[hours] decimal(3,1)	Not Null,	
 
 	Constraint [FK_Works_SSN] Foreign Key (ESSN) References Employee(SSN),
